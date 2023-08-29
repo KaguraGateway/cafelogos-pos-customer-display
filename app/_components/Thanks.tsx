f5858467345055dc26098998f6cac1cd3911df6e
KaguraGateway <32214018+KaguraGateway@users.noreply.github.com>
2023-08-29 23:04:55 +0900

    feat: Thanksページを作成

---
+++ app/_components/Thanks.tsx
@@ -0,0 +1,43 @@
+import { Container, Flex, Box, Text, Table, TableContainer, Thead, Tr, Th, Tbody, Td } from '@chakra-ui/react';
+import { CheckCircleIcon } from '@chakra-ui/icons';
+import { Yen } from '@/ui/Yen';
+
+export function Thanks() {
+  return (
+    <Container maxW="container.md" position="absolute" top={0} bottom={0} left={0} right={0}>
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        w="100%"
+        my={{ base: '32px', lg: '64px' }}
+      >
+        <Box>
+          <CheckCircleIcon w={{ base: '6rem', lg: '12rem' }} h={{ base: '6rem', lg: '12rem' }} color="green.500" />
+        </Box>
+        <Box mt="16px">
+          <Text fontSize={{ base: '4xl', lg: '6xl' }}>ありがとうございました</Text>
+        </Box>
+        <Box w="100%" mt={{ base: '32px', lg: '64px' }}>
+          <Flex
+            justifyContent="space-between"
+            borderBottom="1px"
+            mb={{ base: '16px', lg: '24px' }}
+            pb={{ base: '16px', lg: '24px' }}
+          >
+            <Text fontSize={{ base: '3xl', lg: '5xl' }}>合計</Text>
+            <Text fontSize={{ base: '3xl', lg: '5xl' }}>
+              <Yen yen={1000} />
+            </Text>
+          </Flex>
+          <Flex justifyContent="space-between">
+            <Text fontSize={{ base: '3xl', lg: '5xl' }}>おつり</Text>
+            <Text fontSize={{ base: '3xl', lg: '5xl' }}>
+              <Yen yen={500} />
+            </Text>
+          </Flex>
+        </Box>
+      </Flex>
+    </Container>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,26 @@ import { useState } from 'react';
 import { Ads } from './_components/Ads';
 import { AddItem } from './_components/AddItem';
 import { Payment } from './_components/Payment';
+import { Thanks } from './_components/Thanks';
 
 const StateType = {
   ads: 0,
   addItem: 1,
   payment: 2,
+  thanks: 3,
 } as const;
 type StateType = (typeof StateType)[keyof typeof StateType];
 
 export default function Home() {
-  const [state, setState] = useState<StateType>(StateType.payment);
+  const [state, setState] = useState<StateType>(StateType.thanks);
 
-  if (state == StateType.ads) {
-    return <Ads />;
-  } else if (state == StateType.addItem) {
+  if (state == StateType.addItem) {
     return <AddItem />;
   } else if (state == StateType.payment) {
     return <Payment />;
+  } else if (state == StateType.thanks) {
+    return <Thanks />;
   }
 
-  return <main></main>;
+  return <Ads />;
 }
